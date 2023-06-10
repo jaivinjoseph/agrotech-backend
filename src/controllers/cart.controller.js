2dca07aed61709920f97531f6daa4e759c4bbd27
pranav madhu <pranavmadhu73@gmail.com>
2023-06-10 18:49:42 +0530

    included function for deleting cart items

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,4 +1,5 @@
 import CartSchema from "../models/cart.model.js";
+import ProductSchema from "../models/product.model.js";
 const addCartData = async (req, res, next) => {
   const data = req.body;
   try {
@@ -17,6 +18,8 @@ const addCartData = async (req, res, next) => {
 
 const getCartDatByUserId = async (req, res, next) => {
   try {
+    const resultData = [];
+    let grandTotal = 0;
     const cartData = await CartSchema.find({ user_id: req.params.user_id });
     if (cartData.length <= 0) {
       res.status(404).json({
@@ -25,7 +28,22 @@ const getCartDatByUserId = async (req, res, next) => {
           "No cart data found for the user,add something new to the cart",
       });
     } else {
-      res.status(200).json({ success: true, data: cartData });
+      await Promise.all(
+        cartData.map(async ({ user_id, product_id, selectedquantity, _id }) => {
+          let product = await ProductSchema.findById(product_id);
+          resultData.push({
+            product: product,
+            selectedquantity: selectedquantity,
+            _id: _id,
+          });
+          grandTotal = grandTotal + product.price * selectedquantity;
+        })
+      );
+      console.log(resultData);
+      res.status(200).json({
+        success: true,
+        data: { data: resultData, grandtotal: grandTotal },
+      });
     }
   } catch (error) {
     res.status(500).json({
@@ -35,4 +53,18 @@ const getCartDatByUserId = async (req, res, next) => {
   }
 };
 
-export { addCartData, getCartDatByUserId };
+const deleteCartData = async (req, res, next) => {
+  try {
+    await CartSchema.findByIdAndDelete(req.params.item_id);
+    res
+      .status(200)
+      .json({ success: true, message: "CART ITEM DELETED SUCCESSFULLY" });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error ,try again later",
+    });
+  }
+};
+
+export { addCartData, getCartDatByUserId, deleteCartData };
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,9 +1,11 @@
-  import express from "express";
+import express from "express";
 import {
   addCartData,
+  deleteCartData,
   getCartDatByUserId,
 } from "../controllers/cart.controller.js";
 const router = express.Router();
 router.post("/add", addCartData);
 router.get("/user/:user_id", getCartDatByUserId);
+router.delete("/cart/:item_id", deleteCartData);
 export default router;
